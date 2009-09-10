30a8c5f6116b3acf38050d7a32152fed924b88a9
Pelle Braendgaard <pelleb@gmail.com>
2009-09-10 14:20:58 -0400

    Improved the display of transactions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  # 
+  def transaction_link(t)
+    counterparty=t['to']==current_card.email ? t['from'] : t['to']
+    if counterparty==OPERATOR_ACCOUNT
+      counterparty=t["memo"]
+    end
+    amount=t['amount'].to_f*(t['to']==current_card.email ? 1 : -1 )
+    link_to "#{counterparty} - #{number_to_currency(amount)}",t["txn_id"]
+  end
 end
