14fea2003fb46914678339b37e22ab53ea61b2d5
Pelle Braendgaard <pelleb@gmail.com>
2009-09-10 13:05:41 -0400

    Fixed the reason why emails weren't being sent. Apparently in new version of rails the sender email has to be just the email without angel brackets

--- app/models/registration_notifier.rb
+++ app/models/registration_notifier.rb
@@ -4,7 +4,7 @@ class RegistrationNotifier < ActionMailer::Base
   def credentials(card,sent_at = Time.now)
     subject    'Here is your new CapCard'
     recipients "#{card.name} <#{card.email}>"
-    from       'CapCard <capcard@stakeventures.com>'
+    from       OPERATOR_ACCOUNT
     sent_on    sent_at
     
     body       :card=>card
